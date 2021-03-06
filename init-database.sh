332bd3de9104ed64827d997051a4b23d6fd15127
ylaksun@yahoo.com <ylaksun@yahoo.com>
2021-03-06 15:36:03 +0000

    docker-compose

---
+++ init-database.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER docker;
+    CREATE DATABASE cybertek;
+    GRANT ALL PRIVILEGES ON DATABASE cybertek TO docker;
+    CREATE DATABASE my_project_test;
+    GRANT ALL PRIVILEGES ON DATABASE my_project_test TO docker;
+EOSQL
